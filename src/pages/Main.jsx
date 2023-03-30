018187005a33f3027b5b95d30346836ef97815f5
Kolizey_08 <magamedbagiev@gmail.com>
2023-03-30 16:35:51 +0300

    Сделал новое окно

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import Confidence from "../components/Confidence/Confidence";
 import Home from "../components/Home/Home";
 import Services from "../components/Services/Services";
+import Trealment from "../components/Trealment/Trealment";
 
 import styles from "./Main.module.scss";
 
@@ -12,6 +13,7 @@ const Main = () => {
         <Home />
         <Services />
         <Confidence />
+        <Trealment />
       </div>
     </div>
   );
